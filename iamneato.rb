b4a283657820d494fe3bdd4026e7f54bac6ad564
Emilio Cavazos <Emilio@emilio-cavazoss-macbook.local>
2009-07-12 15:08:17 -0700

    git init

---
+++ iamneato.rb
@@ -0,0 +1,67 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'time'
+require 'lib/article'
+
+Article.path = File.join(Sinatra::Application.root, 'articles')
+
+helpers do
+  def article_body(article)
+    haml(article.template, :layout => false)
+  end
+
+  def article_path(article)
+    "/#{article.published.strftime("%Y/%m/%d")}/#{article.id}"
+  end
+  
+  def absoluteify_links(html)
+    html.
+      gsub(/href=(["'])(\/.*?)(["'])/, 'href=\1http://hughevans.net\2\3').
+      gsub(/src=(["'])(\/.*?)(["'])/, 'src=\1http://hughevans.net\2\3')
+  end
+end
+
+get '/' do
+  @articles = Article.all.sort[0..10]
+  haml :home
+end
+
+get '/:year/:month/:day/:id' do
+  @article = Article[params[:id]] || raise(Sinatra::NotFound)
+  @title = @article.title
+  haml :article
+end
+
+get '/doodles' do
+  @title = 'Doodles'
+  haml :doodles
+end
+
+get '/archive' do
+  @articles = Article.all.sort
+  @title = 'Archive'
+  haml :archive
+end
+
+get '/about' do
+  @title = 'About'
+  haml :about
+end
+
+get '/articles.atom' do
+  @articles = Article.all.sort
+  content_type 'application/atom+xml'
+  haml :feed, :layout => false
+end
+
+get '/:style.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :"stylesheets/#{params[:style]}"
+end
+
+module Haml::Filters::Preserve
+  def render(text)
+    Haml::Helpers.preserve(Haml::Helpers.html_escape(text))
+  end
+end
---
+++ public/javascripts/fancybox.init.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+	$("#doodles a").fancybox();
+
+	$("a.group").fancybox({
+		'zoomSpeedIn':	300, 
+		'zoomSpeedOut':	300, 
+		'overlayShow':	false
+	});
+});
