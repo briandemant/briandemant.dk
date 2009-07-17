653118fea22e0798b09c6559211447f2b135ec75
ecavazos <ejcavazos@gmail.com>
2009-07-17 06:33:19 -0700

    adding article_getter + spec

---
+++ lib/article_getter.rb
@@ -0,0 +1,33 @@
+
+
+class ArticleGetter
+  
+  def initialize(path_to_articles)
+    @base_path = path_to_articles
+  end
+  
+  def article_path(article_slug = nil)
+    article_slug ? File.join(@base_path, "#{article_slug}.haml") : @base_path
+  end
+
+  def files
+    Dir["#{File.expand_path(@base_path)}/*.haml"]
+  end
+
+  def all
+    files.map {|f| Article.new(f, File.read(f))}
+  end
+
+  def [](id)
+    path = article_path(id.gsub('-', '_'))
+    File.exist?(path) && Article.new(path, File.read(path))
+  end
+
+  def template_variable(text, name)
+    text[/\-\s*#\s*#{name}:\s*(.+)/, 1]
+  end
+
+  def parse_date(date_string)
+    date_string && Time.local(*date_string.split('-').map {|s| s.to_i})
+  end
+end
--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -1,32 +1,35 @@
 require File.dirname(__FILE__) + '/spec_helper'
-require File.dirname(__FILE__) + '/../lib/article_getter'
 
-describe "ArticleGetter" do
+describe "Article" do
   
   before(:each) do
-    @getter = ArticleGetter.new('articles')
+    Article.path = 'articles'
+  end
+  
+  it "should return path to articles" do
+    Article.path.should == 'articles'
   end
   
   it "should retrun all article file names" do
-    f = @getter.files[0]
+    f = Article.files[0]
     File.extname(f).should == '.haml'
   end
   
   it "should return all Articles" do
-    @getter.all[0].instance_of?(Article).should be_true
+    Article.all[0].instance_of?(Article).should be_true
   end
   
   it "should return a file by name (aka ID) when accessing via an indexer" do
-    @getter['view-first'].title.should == 'View First'
+    Article['view-first'].title.should == 'View First'
   end
-
+  
   it "should be able to parse 'title' out of a line up" do
     fake_article = "blah -#title: Fake Title\n\r some new shizzle"
-    @getter.template_variable(fake_article, 'title').should == 'Fake Title'
+    Article.template_variable(fake_article, 'title').should == 'Fake Title'
   end
   
   it "should return a date" do
-    @getter.parse_date('2009-11-12').instance_of?(Time).should be_true
+    Article.parse_date('2009-11-12').instance_of?(Time).should be_true
   end
-
+  
 end
